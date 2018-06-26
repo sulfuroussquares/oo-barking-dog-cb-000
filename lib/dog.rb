f09445377f196dbf7a1baba97bb9612cc52ee4c6
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-26 01:29:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
 class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "Bark!"
+  end
+  
+
 end
